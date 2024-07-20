d778e82b9713179f5b426853df6e921884d2cf38
Gulshan Mathur <gulshanmathur556@gmail.com>
2024-07-20 15:07:42 +0530

    useform hook use to and apply validation of hook

--- src/features/auth/components/Login.jsx
+++ src/features/auth/components/Login.jsx
@@ -1,4 +1,4 @@
-// import  React,{ useState } from "react";
+import  React,{ useState } from "react";
 import {NavLink} from "react-router-dom"
 export default function Login() {
   return (
--- src/features/auth/components/Signup.jsx
+++ src/features/auth/components/Signup.jsx
@@ -1,6 +1,13 @@
 import { NavLink } from "react-router-dom";
-
+import {useForm} from "react-hook-form";
 function Signup() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  console.log({errors});
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -15,7 +22,7 @@ function Signup() {
     </div>
 
     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form className="space-y-6" action="#" method="POST">
+      <form noValidate className="space-y-6" onSubmit={handleSubmit((data) => console.log(data))} >
         <div>
           <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
             Email address
@@ -23,12 +30,11 @@ function Signup() {
           <div className="mt-2">
             <input
               id="email"
-              name="email"
+              {...register("email",{ required: "email is required!" })}
               type="email"
-              autoComplete="email"
-              required
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
+           {errors.email && <p className="text-red-500">{errors.email.message}</p>}
           </div>
         </div>
 
@@ -41,11 +47,11 @@ function Signup() {
           <div className="mt-2">
             <input
               id="password"
-              name="password"
+              {...register("password",{ required: "password is required!" })}
               type="password"
-              required
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
+            {errors.password && <p className="text-red-500">{errors.password.message}</p>}
           </div>
           <div className="flex items-center justify-between">
             <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
@@ -54,12 +60,12 @@ function Signup() {
           </div>
           <div className="mt-2">
             <input
-              id="confirm-password"
-              name="confirm-password"
+              id="confirmPassword"
+              {...register("confirmPassword",{ required: "confirm password is required!" })}
               type="password"
-              required
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
+            {errors.confirmPassword && <p className="text-red-500">{errors.confirmPassword.message}</p>}
           </div>
         </div>
 
